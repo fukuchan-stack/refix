8f481385d008461b50ee40d8facae4502cb70de1
Yuseke Fukuyama <fukuchan.1109bi@gmail.com>
2025-08-30 20:59:30 +0900

    chore: Remove temporary linter test file

--- test_linter.py
+++
@@ -1,6 +0,0 @@
-# test_linter.py
-import os # Flake8 will complain: 'os' imported but unused
-
-def my_function( a,b ): # Flake8 will complain: missing whitespace around operator
-    print("Hello from the test linter file!")
-    return a+b
